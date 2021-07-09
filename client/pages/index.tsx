dcd1d4fee836b80142d54e389a985c0e2505dcf8
SkullCutter02 <64243477+SkullCutter02@users.noreply.github.com>
2021-07-09 20:13:56 +0800

    Set up mantine

---
+++ client/pages/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Center } from "@mantine/core";
+
+const HomePage: React.FC = () => {
+  return (
+    <>
+      <Center>
+        <p>Hello</p>
+      </Center>
+    </>
+  );
+};
+
+export default HomePage;
