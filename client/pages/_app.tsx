1f01ac61042b408b1702e71bca0fca1b68e7ffc8
SkullCutter02 <64243477+SkullCutter02@users.noreply.github.com>
2021-07-10 23:25:17 +0800

    Set up react query

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,5 +1,9 @@
+import React, { useState } from "react";
 import { AppProps } from "next/app";
 import Head from "next/head";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import { Hydrate } from "react-query/hydration";
 
 import Navbar from "../components/layout/Navbar";
 
@@ -8,24 +12,28 @@ import "../styles/global.css";
 export default function App(props: AppProps) {
   const { Component, pageProps } = props;
 
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
     <>
-      <Head>
-        <title>Ling Handmade</title>
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width"
-        />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <Head>
+            <title>Ling Handmade</title>
+            <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" />
+            <link
+              href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+              rel="stylesheet"
+            />
+          </Head>
 
-      <Navbar />
-      <Component {...pageProps} />
+          <Navbar />
+          <Component {...pageProps} />
+          <ReactQueryDevtools />
+        </Hydrate>
+      </QueryClientProvider>
     </>
   );
 }
