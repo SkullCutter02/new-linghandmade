e5036d6fc196179309ca7dc861bbe4abf19242dd
SkullCutter02 <64243477+SkullCutter02@users.noreply.github.com>
2021-07-15 19:45:39 +0800

    Set up react query with admin

--- admin/pages/_app.tsx
+++ admin/pages/_app.tsx
@@ -1,7 +1,11 @@
+import { useState } from "react";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import { ChakraProvider } from "@chakra-ui/react";
 import { useRouter } from "next/router";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { Hydrate } from "react-query/hydration";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 import DashboardLayout from "../components/layout/DashboardLayout";
 
@@ -13,22 +17,33 @@ export default function App(props: AppProps) {
   const router = useRouter();
   const path = router.asPath.split("/");
 
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
     <>
-      <ChakraProvider>
-        <Head>
-          <title>Ling Handmade Admin</title>
-          <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-        </Head>
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <ChakraProvider>
+            <Head>
+              <title>Ling Handmade Admin</title>
+              <meta
+                name="viewport"
+                content="minimum-scale=1, initial-scale=1, width=device-width"
+              />
+            </Head>
 
-        {path[1] !== "" ? (
-          <DashboardLayout>
-            <Component {...pageProps} />
-          </DashboardLayout>
-        ) : (
-          <Component {...pageProps} />
-        )}
-      </ChakraProvider>
+            {path[1] !== "" ? (
+              <DashboardLayout>
+                <Component {...pageProps} />
+              </DashboardLayout>
+            ) : (
+              <Component {...pageProps} />
+            )}
+          </ChakraProvider>
+
+          <ReactQueryDevtools />
+        </Hydrate>
+      </QueryClientProvider>
     </>
   );
 }
