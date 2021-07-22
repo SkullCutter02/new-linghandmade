cf5e63727d0a3821c6531eaf0041c4abc3069b63
SkullCutter02 <64243477+SkullCutter02@users.noreply.github.com>
2021-07-22 22:59:59 +0800

    Created separated component for footer form

--- client/components/layout/Footer.tsx
+++ client/components/layout/Footer.tsx
@@ -4,18 +4,13 @@ import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope, faMobileAlt } from "@fortawesome/free-solid-svg-icons";
 import Link from "next/link";
 
+import FooterForm from "./FooterForm";
+
 const Footer: React.FC = () => {
   return (
     <>
       <footer>
-        <form>
-          <h3>Send Us An Email</h3>
-          <input type="text" placeholder={"Your Name..."} />
-          <input type="text" placeholder={"Your Email..."} />
-          <input type="text" placeholder={"Subject..."} />
-          <textarea placeholder={"Message..."} />
-          <button type={"submit"}>SEND</button>
-        </form>
+        <FooterForm />
         <div className="upper">
           <h2>Contact Us</h2>
           <p className="contact-us-description">
@@ -63,57 +58,6 @@ const Footer: React.FC = () => {
         footer {
           border-top: 1px solid var(--secondaryColor);
           position: relative;
-        }
-
-        form {
-          display: flex;
-          flex-direction: column;
-          justify-content: space-between;
-          position: absolute;
-          top: 110px;
-          right: 15%;
-          box-shadow: 0 0 20px 2px #c4c4c4;
-          width: 33%;
-          padding: 60px 50px;
-          height: 560px;
-          background: #fff;
-        }
-
-        form h3 {
-          color: var(--primaryTextColor);
-          text-transform: uppercase;
-        }
-
-        form input,
-        form textarea {
-          border: 1px solid var(--primaryColor);
-          resize: none;
-          padding: 10px;
-          font-size: 0.8rem;
-        }
-
-        form input:invalid {
-          border: 1px solid red;
-        }
-
-        form textarea {
-          height: 150px;
-        }
-
-        form button {
-          width: 100%;
-          height: 40px;
-          border: 1px solid var(--primaryColor);
-          background: var(--primaryColor);
-          box-shadow: inset 0 0 0 0.09px #fff;
-          color: #fff;
-          font-size: 1rem;
-          transition: all 0.4s;
-        }
-
-        form button:hover:not(:disabled) {
-          box-shadow: inset 500px 0 0 0.09px #fff;
-          color: var(--primaryColor);
         }
 
         .upper,
@@ -188,13 +132,6 @@ const Footer: React.FC = () => {
           text-decoration: underline;
         }
 
-        @media screen and (max-width: 900px) {
-          form {
-            width: 40%;
-            right: 5%;
-          }
-        }
-
         @media screen and (max-width: 690px) {
           .upper,
           .lower {
@@ -208,13 +145,6 @@ const Footer: React.FC = () => {
 
           .lower {
             padding-top: 270px;
-          }
-
-          form {
-            right: 50%;
-            width: 75%;
-            transform: translate(50%, 0);
-            top: 470px;
           }
         }
       `}</style>
---
+++ client/components/layout/FooterForm.tsx
@@ -0,0 +1,87 @@
+import React from "react";
+
+const FooterForm: React.FC = () => {
+  return (
+    <>
+      <form>
+        <h3>Send Us An Email</h3>
+        <input type="text" placeholder={"Your Name..."} />
+        <input type="text" placeholder={"Your Email..."} />
+        <input type="text" placeholder={"Subject..."} />
+        <textarea placeholder={"Message..."} />
+        <button type={"submit"}>SEND</button>
+      </form>
+
+      <style jsx>{`
+        form {
+          display: flex;
+          flex-direction: column;
+          justify-content: space-between;
+          position: absolute;
+          top: 110px;
+          right: 15%;
+          box-shadow: 0 0 20px 2px #c4c4c4;
+          width: 33%;
+          padding: 60px 50px;
+          height: 560px;
+          background: #fff;
+        }
+
+        form h3 {
+          color: var(--primaryTextColor);
+          text-transform: uppercase;
+        }
+
+        form input,
+        form textarea {
+          border: 1px solid var(--primaryColor);
+          resize: none;
+          padding: 10px;
+          font-size: 0.8rem;
+        }
+
+        form input:invalid {
+          border: 1px solid red;
+        }
+
+        form textarea {
+          height: 150px;
+        }
+
+        form button {
+          width: 100%;
+          height: 40px;
+          border: 1px solid var(--primaryColor);
+          background: var(--primaryColor);
+          box-shadow: inset 0 0 0 0.09px #fff;
+          color: #fff;
+          font-size: 1rem;
+          transition: all 0.4s;
+        }
+
+        form button:hover:not(:disabled) {
+          box-shadow: inset 500px 0 0 0.09px #fff;
+          color: var(--primaryColor);
+        }
+
+        @media screen and (max-width: 900px) {
+          form {
+            width: 40%;
+            right: 5%;
+          }
+        }
+
+        @media screen and (max-width: 690px) {
+          form {
+            right: 50%;
+            width: 75%;
+            transform: translate(50%, 0);
+            top: 470px;
+          }
+        }
+      `}</style>
+    </>
+  );
+};
+
+export default FooterForm;
