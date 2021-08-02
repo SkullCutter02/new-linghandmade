0cd5e7ee43b8e64fda72d935ef093bbe6f55f115
skullcutter02 <=>
2021-08-02 20:27:55 +0800

    Implemented full text search in postgres

--- server/src/order/order.service.ts
+++ server/src/order/order.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from "@nestjs/common";
+import { EntityManager } from "@mikro-orm/postgresql";
 
 import { OrderRepository } from "./repositories/order.repository";
 import { CreateOrderDto } from "./dto/createOrder.dto";
@@ -6,17 +7,45 @@ import { CartService } from "../user/cart/cart.service";
 import { UserInCartProducts } from "../user/entities/userInCartProducts.entity";
 import { User } from "../user/entities/user.entity";
 import { PaginationDto } from "../shared/pagination.dto";
+import { Order } from "./entities/order.entity";
 
 @Injectable()
 export class OrderService {
-  constructor(private readonly orderRepository: OrderRepository, private readonly cartService: CartService) {}
+  constructor(
+    private readonly orderRepository: OrderRepository,
+    private readonly cartService: CartService,
+    private readonly em: EntityManager,
+  ) {}
 
   async find({ page, limit, filter }: PaginationDto) {
-    const [orders, count] = await this.orderRepository.findAndCount(
-      { name: { $ilike: `%${filter}%` } },
-      { limit, offset: (page - 1) * limit, orderBy: { createdAt: "ASC" } },
-    );
-    return { orders, hasMore: count > page * limit };
+    if (!filter) {
+      const [orders, count] = await this.orderRepository.findAndCount(
+        { name: { $ilike: `%${filter}%` } },
+        { limit, offset: (page - 1) * limit, orderBy: { createdAt: "ASC" } },
+      );
+      return { orders, hasMore: count > page * limit };
+    } else {
+      const orders = await this.em
+        .createQueryBuilder(Order)
+        .select("*")
+        .where(
+          "to_tsvector(name || ' ' || address || ' ' || email || ' ' || phone_number) @@ plainto_tsquery(?)",
+          [filter],
+        )
+        .limit(limit)
+        .offset((page - 1) * limit)
+        .execute("all");
+      const count = await this.em
+        .createQueryBuilder(Order)
+        .select("*")
+        .where(
+          "to_tsvector(name || ' ' || address || ' ' || email || ' ' || phone_number) @@ plainto_tsquery(?)",
+          [filter],
+        )
+        .count()
+        .execute();
+      return { orders, hasMore: count > page * limit };
+    }
   }
 
   async create({ name, address, phoneNumber }: CreateOrderDto, user: User) {
