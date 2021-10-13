97fede99f827b72088014901408add3a05a79bd3
skullcutter02 <=>
2021-10-13 21:50:31 +0800

    Made ormconfig production ready

--- server/src/config/ormconfig.ts
+++ server/src/config/ormconfig.ts
@@ -3,9 +3,11 @@ import { SqlHighlighter } from "@mikro-orm/sql-highlighter";
 
 const ormconfig: MikroOrmModuleSyncOptions = {
   type: "postgresql",
-  host: "localhost",
-  port: 5432,
-  dbName: "new_linghandmade_db",
+  host: process.env.PG_HOST || "localhost",
+  port: parseInt(process.env.PG_PORT) || 5432,
+  dbName: process.env.PG_NAME || "new_linghandmade_db",
+  user: process.env.USER || null,
+  password: process.env.PG_PASSWORD || null,
   baseDir: __dirname,
   entities: ["../**/*.entity.js"],
   entitiesTs: ["../**/*.entity.ts"],
