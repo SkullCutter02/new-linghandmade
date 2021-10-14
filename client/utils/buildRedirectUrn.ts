2fff080af3803d225b8f54280f61fcb720e3567d
skullcutter02 <=>
2021-10-14 10:48:42 +0800

    Fixed redirect problem in client

--- client/utils/buildRedirectUrn.ts
+++ client/utils/buildRedirectUrn.ts
@@ -1,6 +1,8 @@
 const buildRedirectUrn = (query: object) => {
   let s = "";
 
+  if (Object.keys(query).length === 0) return "/";
+
   for (const [key, value] of Object.entries(query)) {
     if (key === "redirect") s += value;
     else s += `&${key}=${value}`;
